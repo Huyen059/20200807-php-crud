5e0fc47b10793a5c46e252d3659a39b821eaec1a
Huyen059 <samihuyen059@gmail.com>
2020-08-08 23:41:55 +0200

    can display class in teacher general and detail pages

--- Model/TeacherLoader.php
+++ Model/TeacherLoader.php
@@ -14,7 +14,7 @@ class TeacherLoader
 
     public function __construct(\PDO $pdo)
     {
-        $handle = $pdo->prepare('SELECT * FROM teacher');
+        $handle = $pdo->prepare('SELECT teacher.id, firstName, lastName, email, teacher.address, class.id as classId, class.name as className, class.address as classAddress FROM teacher LEFT JOIN class ON teacher.id = class.teacher_id');
         $handle->execute();
         $teachers = $handle->fetchAll();
 
@@ -26,6 +26,10 @@ class TeacherLoader
             $newTeacher = new Teacher($teacher['firstName'], $teacher['lastName'], $teacher['email'], $teacher['address']);
             $newTeacher->setId((int)$teacher['id']);
             $this->teachers[$teacher['id']] = $newTeacher;
+            if($teacher['classId']) {
+                $class = new LearningClass($teacher['className'], $teacher['classAddress']);
+                $newTeacher->setClass($class);
+            }
         }
     }
 
