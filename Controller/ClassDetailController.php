efdee6ef0dda96fe223b1efdbf2d7472704e0bab
Huyen059 <samihuyen059@gmail.com>
2020-08-08 14:50:41 +0200

    can view class details

--- Controller/ClassDetailController.php
+++ Controller/ClassDetailController.php
@@ -1,11 +1,25 @@
 <?php
 declare(strict_types=1);
 namespace Controller;
+use Model\ClassLoader;
+use Model\ClassLoaderException;
+
 ini_set('display_errors', "1");
 ini_set('display_startup_errors', "1");
 error_reporting(E_ALL);
 
 class ClassDetailController
 {
+    public function render()
+    {
+        try {
+            $loader = new ClassLoader();
+            $class = $loader->getClasses()[(int)$_GET['id']];
+        }
+        catch (ClassLoaderException $e) {
+            $errorMessage = $e->getMessage();
+        }
 
+        require 'View/class_detail.php';
+    }
 }
--- View/class_detail.php
+++ View/class_detail.php
@@ -1,5 +1,71 @@
 <?php
 declare(strict_types=1);
+
+use Model\LearningClass;
+
 ini_set('display_errors', "1");
 ini_set('display_startup_errors', "1");
 error_reporting(E_ALL);
+?>
+
+<?php require 'includes/header.php'; ?>
+
+<section class="container">
+    <h1 class="text-center mb-5">Class Details</h1>
+
+    <div class="mb-5">
+        <form method="post" class="d-flex justify-content-center">
+            <input type="hidden" name="action" value="delete">
+            <button class="btn btn-danger" name="delete" type="submit">Delete</button>
+        </form>
+    </div>
+
+    <?php if(isset($errorMessage)): ?>
+        <div class='alert alert-success my-4 text-center' role='alert'>
+            <?= $errorMessage ?>
+        </div>
+    <?php else: ?>
+        <div>
+            <table class="table">
+                <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Address</th>
+                    <th scope="col">Teacher</th>
+                    <th scope="col">Students</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <?php /** @var LearningClass $class */?>
+                    <th scope="row" class="align-middle"><?= $class->getId() ?></th>
+                    <td class="align-middle"><?= $class->getName() ?></td>
+                    <td class="align-middle"><?= $class->getAddress() ?></td>
+                    <td class="align-middle"><?php if($class->getTeacher() !== null){
+                            echo $class->getTeacher()->getFullName();
+                        } else {
+                            echo "N/A";
+                        }
+                        ?>
+                    </td>
+                    <td class="align-middle"><?php if ($class->getName() !== null) {
+                            //Todo: get students and put them here in a list
+                            echo "<a href='#'>{$class->getName()}</a>";
+                        } else {
+                            echo "N/A";
+                        }
+                        ?>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    <?php endif; ?>
+
+    <div class="d-flex justify-content-center">
+        <a href="?page=class" class="btn btn-primary">Back to overview</a>
+    </div>
+</section>
+
+<?php require 'includes/footer.php'; ?>
